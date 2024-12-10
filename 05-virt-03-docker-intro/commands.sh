4b07abf0c324c289abba1d689b899672f0941faf
Solovev Alexander <solovyov_alex@mail.ru>
2024-12-10 14:27:49 +0500

    new

--- 05-virt-03-docker-intro/commands.sh
+++ 05-virt-03-docker-intro/commands.sh
@@ -8,3 +8,6 @@ docker push hachubra/custom_nginx:1.0.0
 
 #2
 docker run -d -p 127.0.0.1:8081:80 --name "solovev-alex-nginx-t2" hachubra/custom_nginx:1.0.0 
+docker rename solovev-alex-nginx-t2 custom-nginx-t2
+date +"%d-%m-%Y %T.%N %Z" ; sleep 0.150 ; docker ps ; ss -tlpn | grep 127.0.0.1:8081  ; docker logs custom-nginx-t2 -n1 ; docker exec -it custom-nginx-t2 base64 /usr/share/nginx/html/index.html
+
